9f35f07bdfef5f1172d669dc811bcbaac7331cac
wpfoster <fosterwp@gmail.com>
2016-03-06 19:14:11 -0500

    MVC Template
    Added MVC 4.6 Project Template

---
+++ LtcProviderData/LtcProviderData/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LtcProviderData.Startup))]
+namespace LtcProviderData
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
